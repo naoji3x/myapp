25a439baab598016c9dcec5245077a059d8beea3
naoji3x <naoji.3x@gmail.com>
2023-05-10 20:05:31 +0900

    fix: next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const urlPrefix = '/myapp'
+
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: urlPrefix,
+  basePath: urlPrefix,  
 }
 
 module.exports = nextConfig
