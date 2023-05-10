ce6e1145bef167c9915c6f534b7bad7b0deebe74
naoji3x <naoji.3x@gmail.com>
2023-05-10 21:44:34 +0900

    build: fix nextjs.yml

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const urlPrefix = process.env.URL_PREFIX ? '/' + process.env.URL_PREFIX : ''
 
 const nextConfig = {
-  trailingSlash: true,
+  reactStrictMode: true,
   publicRuntimeConfig: { urlPrefix },
 }
 
